a4db12f92cd39dedf158457b06a23cf2db82f6bd
Nicholas Durkin <Nicholas@Nicholass-MacBook-Air.local>
2019-02-04 15:23:40 -0500

    commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express();
+const bodyParser = require("body-parser");
 var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
