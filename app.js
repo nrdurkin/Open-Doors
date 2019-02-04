d66f281954a7f59e21d1b47b12e950bea17a4b57
Nicholas Durkin <Nicholas@Nicholass-MacBook-Air.local>
2019-02-04 15:44:36 -0500

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
-
+ 
 
 app.get('/', function(req, res) {
     res.send('index.html');
@@ -12,7 +12,7 @@ app.get('/about', function(req, res) {
     res.sendFile('about.html', {root: __dirname + '/public'});
 });
 app.get('/projects', function(req, res) {
-    res.sendFile('projects.html', {root: __dirname + '/public'});
+    res.sendFile('projects.htm', {root: __dirname + '/public'});
 });
 
 app.listen(port, function() {
